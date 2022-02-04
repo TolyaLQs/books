fabbe029ee50ea0d23985d38e8cd0a9eb77ba4a7
TolyaLQs <sdtolya@gmail.com>
2022-02-04 23:15:58 +0500

    add output new books

--- books_project/mainapp/views.py
+++ books_project/mainapp/views.py
@@ -1,25 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
+from books_project.mainapp.models import Book
 
 
 def index(request):
-    return render(request, 'mainapp/main_page.html')
+    if request.POST:
+        if 'search' in request.POST and request.POST['search']:
+            search_text = request.POST['search']
+            request.session['search'] = search_text
+            return HttpResponseRedirect(reverse('mainapp:search'))
+
+    books_new = Book.objects.all().order_by('-year_publishing')
+    context = {
+        'books_new': books_new,
+    }
+    return render(request, 'mainapp/main_page.html', context)
 
 
 def genres(request):
+    context = {}
     return render(request, 'mainapp/top_gen_new_pop_pages.html')
 
 
 def genre(request, genre=None):
+    context = {}
     return render(request, 'mainapp/top_gen_new_pop_pages.html')
 
 
 def top(request, top=None):
+    context = {}
     return render(request, 'mainapp/top_gen_new_pop_pages.html')
 
 
 def pop(request, top=None):
+    context = {}
     return render(request, 'mainapp/top_gen_new_pop_pages.html')
 
 
@@ -28,10 +44,15 @@ def new(request):
     return render(request, 'mainapp/top_gen_new_pop_pages.html', context)
 
 
-def search(request, search=None):
+def search(request):
+    search_text = request.session.get('search', '-')
+    books = Book.objects.filter(name=search_text)
+    context = {
+        'books': books,
+    }
     return render(request, 'mainapp/search_page.html')
 
 
 def book(request, book=None):
+    context = {}
     return render(request, 'mainapp/book_page.html')
-
